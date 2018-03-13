3f539a47632fcc09a5c37b49e41139657373c3ec
Chris Parr <parr.cj@gmail.com>
2018-03-13 18:49:41 +0000

    up to autolayout

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
 }
 
