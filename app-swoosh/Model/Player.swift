36d20c331f8ea47dc60382196a393c2530728302
Chris Parr <parr.cj@gmail.com>
2018-03-17 19:00:00 +0000

    VC data pass

---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Christopher Parr on 17/03/2018.
+//  Copyright © 2018 Christopher Parr. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+    
+}
